05369883e641dc02cf7af0f8dc6e944cfb38a1d5
Mostafa-Anwar-101 <mostafa.mohamed.anwar13@gmail.com>
2025-03-20 14:09:36 +0200

    refactor : connect_to_database use list instead of entry

--- connect_to_database.sh
+++ connect_to_database.sh
@@ -4,26 +4,23 @@ source utils.sh
 source ./tables_menu.sh
 
 connect_to_database() {
-    db_name=$(zenity --entry --title="Connect to Database" --text="Enter database name:" --width=500 --height=450)
-    
+    local db_path="databases"
 
-    if [ $? -eq 1 ];then
-            pacMan "....Exiting....  " 0.05 "."
-            echo "Thanks for using our Database Engine "
-            exit
+    if ! is_db_dir_exist "$db_path"; then
+    zenity --error --text="No databases available to drop."
+    return
     fi
 
-    # local db_list=()
-    # for db in "$db_path"/*; do
-    #     [[ -d "$db" ]] && db_list+=("$(basename "$db")")
-    # done
+    local db_list=()
+    for db in "$db_path"/*; do
+        [[ -d "$db" ]] && db_list+=("$(basename "$db")")
+    done
 
-    # db_name=$(zenity --list --title="Connect to Database" --column="Databases" --width=500 --height=450 )
-     
-     pwd
+    db_name=$(zenity --list --title="Connect to Database" --column="Databases" --width=500 --height=450 "${db_list[@]}")
+
     if [[ -n "$db_name" ]]; then
         zenity --info --text="Connected to database '$db_name'."
-        cd "./databases/$db_name" || exit
+        cd "$db_path/$db_name" || exit
         tables_menu
     fi
 }
